6514729adca1c13bd5a139677adcd2ccd1f3a55f
Alex Willisson <atw@mit.edu>
2012-12-23 04:17:54 -0500

    combined cmd and listen

--- listen.py
+++ listen.py
@@ -4,11 +4,13 @@
 import sys
 from ispeech import *
 
-if len (sys.argv) != 2:
-    print ("usage: listen filename")
+if len (sys.argv) <= 1:
+    print ("usage: " + sys.argv[0] + " filename [space separated commands]")
     exit (1)
 
-fn = sys.argv[1]
+cmds = None
+if len (sys.argv) >= 3:
+    cmds = "|".join (sys.argv[2:])
 
 try:
     f = open ("apikey", "r")
@@ -21,14 +23,22 @@ key = f.read ().strip ()
 speechrecognizer = speechrecognizer()
 speechrecognizer.set_parameter("server", "http://api.ispeech.org/api/rest")
 speechrecognizer.set_parameter("apikey", key)
-speechrecognizer.set_parameter("freeform", "3")
 speechrecognizer.set_parameter("locale", "en-US")
 speechrecognizer.set_parameter("output", "json")
 speechrecognizer.set_parameter("content-type", "wav")
-speechrecognizer.set_parameter("audio", speechrecognizer.base64_encode(speechrecognizer.file_get_contents(fn)))
+
+if cmds:
+    speechrecognizer.set_parameter("freeform", "0")
+    speechrecognizer.set_parameter("alias", "command1|CMDS")
+    speechrecognizer.set_parameter("CMDS", cmds)
+    speechrecognizer.set_parameter("command1", "%CMDS%")
+else:
+    speechrecognizer.set_parameter("freeform", "3")
+
+speechrecognizer.set_parameter("audio", speechrecognizer.base64_encode(speechrecognizer.file_get_contents("yes.wav")))
 result = speechrecognizer.make_request()
 
 if isinstance(result, dict): #error occured
     print(result["error"])
 else:
-    print(result)
+    print (result);
