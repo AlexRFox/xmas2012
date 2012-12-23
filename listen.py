58d98a229effc62b1f6abf634fd25d1536122726
Alex Willisson <atw@mit.edu>
2012-12-23 05:00:56 -0500

    got listen.py set up fairly nicely

--- listen.py
+++ listen.py
@@ -1,19 +1,34 @@
 #! /usr/bin/env python3
 # powered by iSpeech(R)
 
-import sys, re
+import sys, re, os
 from ispeech import *
 
 if len (sys.argv) <= 1:
     print ("usage: " + sys.argv[0] + " filename [space separated commands]")
     exit (1)
 
+fn = sys.argv[1]
+ext = os.path.splitext (fn)[1].lower ()[1:]
+
+# known_fmts = ["aiff", "mp3", "ogg", "wma", "flac",
+#               "wav", "alaw", "ulaw", "vox", "mp4"]
+known_fmts = ["wav"]
+
+if not ext:
+    print ("unable to detect format")
+    exit (1)
+elif ext not in known_fmts:
+    print ("format " + ext + " not supported by ispeech")
+    print ("formats supported: " + str (known_fmts))
+    exit (1)
+
 cmds = None
 if len (sys.argv) >= 3:
     for c in sys.argv[2:]:
-        if not re.match ("^[a-zA-Z]+$", c):
+        if not re.match ("^[\w\s]+$", c):
             print ("bad command: " + c)
-            print ("all commands must match ^[a-zA-Z]+$")
+            print ("all commands must match ^[\w\s]+$")
             exit (1)
     cmds = "|".join (sys.argv[2:])
 
@@ -25,12 +40,14 @@ except:
 
 key = f.read ().strip ()
 
+print ("processing " + fn + " as " + ext)
+
 speechrecognizer = speechrecognizer()
 speechrecognizer.set_parameter("server", "http://api.ispeech.org/api/rest")
 speechrecognizer.set_parameter("apikey", key)
 speechrecognizer.set_parameter("locale", "en-US")
 speechrecognizer.set_parameter("output", "json")
-speechrecognizer.set_parameter("content-type", "wav")
+speechrecognizer.set_parameter("content-type", ext)
 
 if cmds:
     speechrecognizer.set_parameter("freeform", "0")
@@ -40,7 +57,7 @@ if cmds:
 else:
     speechrecognizer.set_parameter("freeform", "3")
 
-speechrecognizer.set_parameter("audio", speechrecognizer.base64_encode(speechrecognizer.file_get_contents("yes.wav")))
+speechrecognizer.set_parameter("audio", speechrecognizer.base64_encode(speechrecognizer.file_get_contents(fn)))
 result = speechrecognizer.make_request()
 
 if isinstance(result, dict): #error occured
