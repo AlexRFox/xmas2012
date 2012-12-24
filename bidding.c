da374ea7cc8f88d321fafaf1b4d41471985a72a7
Pace Willisson <pace@alum.mit.edu>
2012-12-24 10:56:04 -0500

    utterance1

--- bidding.c
+++ bidding.c
@@ -11,6 +11,10 @@
 
 int vox_state;
 
+int utterance_avail;
+int utterance_used;
+double *utterance;
+
 void print_stats (void);
 
 int freeze;
@@ -241,6 +245,9 @@ main (int argc, char **argv)
 	sample_rate = 8000;
 	sample_period = 1.0 / sample_rate;
 
+	utterance_avail = sample_rate * 10;
+	utterance = calloc (utterance_avail, sizeof *utterance);
+
 	system_start_secs = get_secs ();
 
 	trace_setup (4);
@@ -270,6 +277,7 @@ struct trace *make_trace (char *name);
 
 double raw_secs;
 double *raw_samps;
+double *raw_ratios;
 int raw_nsamps;
 int raw_offset;
 
@@ -306,6 +314,7 @@ trace_setup (double secs)
 
 	raw_nsamps = raw_secs * sample_rate;
 	raw_samps = calloc (raw_nsamps, sizeof *raw_samps);
+	raw_ratios = calloc (raw_nsamps, sizeof *raw_ratios);
 
 	set_raw_disp_width (100);
 
@@ -348,6 +357,48 @@ double vox_too_quiet_timestamp;
 double vox_too_loud_timestamp;
 double vox_start, vox_end;
 
+double vox_start_debounce = .100;
+
+void
+utterance_start (void)
+{
+	int debounce_samps;
+	int offset;
+	int stop_offset;
+
+	debounce_samps = sample_rate * vox_start_debounce;
+
+	offset = (raw_offset + raw_nsamps - debounce_samps * 2)
+		% raw_nsamps;
+	stop_offset = (raw_offset + 1) % raw_nsamps;
+
+	while (offset != stop_offset) {
+		if (raw_ratios[offset] > 1.1)
+			break;
+		offset = (offset + 1) % raw_nsamps;
+	}
+
+	utterance_used = 0;
+	while (offset != stop_offset && utterance_used < utterance_avail) {
+		utterance[utterance_used++] = raw_samps[offset];
+		offset = (offset + 1) % raw_nsamps;
+	}
+}
+
+void
+utterance_finish (void)
+{
+	FILE *outf;
+	int idx;
+	outf = fopen ("u.dat", "w");
+	for (idx = 0; idx < utterance_used; idx++) {
+		fprintf (outf, "%.6f %g\n", (double)idx / sample_rate,
+			 utterance[idx]);
+	}
+	fclose (outf);
+	printf ("written\n");
+}
+
 void
 process_data (int16_t const *samps, int nsamps)
 {
@@ -361,11 +412,14 @@ process_data (int16_t const *samps, int nsamps)
 		return;
 
 	for (i = 0; i < nsamps; i++) {
+		raw_offset = (raw_offset + 1) % raw_nsamps;
+
 		rawval = samps[i] / 32768.0;
 
 		raw_samps[raw_offset] = rawval;
+		if (vox_state && utterance_used < utterance_avail)
+			utterance[utterance_used++] = rawval;
 
-		raw_offset = (raw_offset + 1) % raw_nsamps;
 		raw_disp_acc += log (rawval * rawval);
 		raw_disp_count++;
 		if (raw_disp_count >= raw_disp_thresh) {
@@ -400,15 +454,20 @@ process_data (int16_t const *samps, int nsamps)
 		if (isnan (ratio))
 		    ratio = 0;
 
+		raw_ratios[raw_offset] = ratio;
+
 		now = get_secs ();
 
 		if (vox_state == 0) {
 			if (ratio < 1.2) {
 				vox_too_quiet_timestamp = now;
-			} else if (now - vox_too_quiet_timestamp > .100) {
+			} else if (now - vox_too_quiet_timestamp
+				   > vox_start_debounce) {
 				vox_state = 1;
 				vox_start = now;
 				vox_too_loud_timestamp = now;
+
+				utterance_start ();
 			}
 		} else {
 			if (ratio > 1.1) {
@@ -417,8 +476,11 @@ process_data (int16_t const *samps, int nsamps)
 				vox_state = 0;
 				vox_end = now;
 				vox_too_quiet_timestamp = now;
+
+				utterance_finish ();
 			}
 		}
+
 
 		if (armed) {
 			fprintf (f_raw, "%.6f %g\n",
